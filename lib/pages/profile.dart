6aea4fe5cd24935c06e3785886d5fb762d0a5d68
Emir <emirmatik16@gmai.com>
2023-12-09 22:35:56 +0300

    google sign in

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tracky/core/user_provider.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile'),
+    User? user = Provider.of<UserProvider>(context).user;
+    return Center(
+      child: Text('User Email: ${user?.email}'),
     );
   }
 }
