65ef3297aa1d3fda3019e5583f04e40d370f0981
Tugce <emirmatik16@gmai.com>
2023-12-13 22:02:48 +0300

    Profile page ui + api connection

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,12 +1,14 @@
-import 'dart:ffi';
+import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tracky/components/styled_button.dart';
 import 'package:tracky/components/styled_input.dart';
 import 'package:tracky/components/styled_text.dart';
 import 'package:tracky/core/user_provider.dart';
+import 'package:http/http.dart' as http;
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -16,17 +18,86 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePage extends State<ProfilePage> {
+  final _emailFormKey = GlobalKey<FormState>();
   late TextEditingController _emailInputController;
-  bool isMailChecked = false;
-  bool isNotificationChecked = false;
+  bool isLoading = true;
+  bool areEqual = true;
   String dropdownValue = 'Light';
+  User? user;
+  Map<String, dynamic> preferences = {
+    'enableAppNotifications': null,
+    'enableEmailNotifications': null,
+    'notifiedEmail': null,
+  };
+  Map<String, dynamic> initialPreferences = {
+    'enableAppNotifications': null,
+    'enableEmailNotifications': null,
+    'notifiedEmail': null,
+  };
+
+  final String serverUrl = 'https://tracky-wwr6.onrender.com';
+
+  void _fetchPreferences() async {
+    final res =
+        await http.get(Uri.parse('$serverUrl/preferences/${user?.uid}'));
+
+    var userPreferences = preferences;
+
+    print(res.body);
+
+    if (res.statusCode == 200) {
+      final body = jsonDecode(res.body) as Map<String, dynamic>;
+      userPreferences = body['data'];
+      _emailInputController.text = userPreferences['notifiedEmail'];
+    }
+
+    setState(() {
+      isLoading = false;
+      preferences = {...userPreferences};
+      initialPreferences = {...userPreferences};
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     _emailInputController = TextEditingController();
-    isMailChecked = false;
-    isNotificationChecked = false;
+    user = Provider.of<UserProvider>(context, listen: false).user;
+    _fetchPreferences();
+  }
+
+  String? _emailValidator(value) {
+    if (value == null ||
+        !RegExp(r'^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$')
+            .hasMatch(value)) {
+      return 'Please type a valid email';
+    }
+
+    return null;
+  }
+
+  void _onChangeEmail(String value) {
+    preferences['notifiedEmail'] = value;
+    bool currentEquality = _arePreferencesEqual();
+
+    if (areEqual != currentEquality) {
+      setState(() {
+        areEqual = currentEquality;
+      });
+    }
+  }
+
+  bool _arePreferencesEqual() {
+    return mapEquals(preferences, initialPreferences);
+  }
+
+  void _onChangeCheckbox(bool? value, String key) {
+    preferences[key] = value;
+    bool currentEquality = _arePreferencesEqual();
+
+    setState(() {
+      areEqual = currentEquality;
+    });
   }
 
   Widget profilePicture(User? user) {
@@ -71,9 +142,7 @@ class _ProfilePage extends State<ProfilePage> {
           fontSize: 16,
           fontWeight: FontWeight.w400,
           height: 1.5,
-          color: Colors.black,
         ),
-        disabledBorder: Theme.of(context).inputDecorationTheme.border,
       ),
     );
   }
@@ -103,7 +172,7 @@ class _ProfilePage extends State<ProfilePage> {
     );
   }
 
-  Widget checkbox(bool check, Function(bool) onChanged) {
+  Widget checkbox(String key) {
     return Transform.scale(
       scale: 1.7,
       child: Checkbox(
@@ -112,9 +181,9 @@ class _ProfilePage extends State<ProfilePage> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(4),
         ),
-        value: check,
+        value: preferences[key],
         onChanged: (bool? value) {
-          onChanged(value ?? false);
+          _onChangeCheckbox(value, key);
         },
       ),
     );
@@ -138,7 +207,6 @@ class _ProfilePage extends State<ProfilePage> {
         border: Border.all(color: Colors.black),
         borderRadius: BorderRadius.circular(8),
       ),
-      padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
       child: Stack(
         alignment: Alignment.centerRight,
         children: [
@@ -146,12 +214,14 @@ class _ProfilePage extends State<ProfilePage> {
             mainAxisSize: MainAxisSize.min,
             children: [
               Icon(Icons.keyboard_arrow_down),
+              SizedBox(width: 8),
             ],
           ),
           SizedBox(
             height: 40,
             width: 171,
             child: DropdownButton<String>(
+              padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
               isDense: true,
               value: dropdownValue,
               onChanged: (String? value) {
@@ -184,6 +254,62 @@ class _ProfilePage extends State<ProfilePage> {
     );
   }
 
+  Widget renderPreferences(User? user) {
+    return isLoading
+        ? const CircularProgressIndicator()
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const StyledText(
+                text: 'Preferences',
+                type: 'h2',
+              ),
+              const SizedBox(height: 28),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const StyledText(text: 'Enable notifications via mail'),
+                  checkbox('enableEmailNotifications'),
+                ],
+              ),
+              const SizedBox(height: 20),
+              const StyledText(text: 'Notified Email'),
+              const SizedBox(height: 7),
+              Form(
+                key: _emailFormKey,
+                autovalidateMode: AutovalidateMode.disabled,
+                child: StyledInput(
+                  controller: _emailInputController,
+                  hint: '${user?.email}',
+                  validatorFn: _emailValidator,
+                  handleChange: _onChangeEmail,
+                ),
+              ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const StyledText(text: 'Enable app notifications'),
+                  checkbox('enableAppNotifications'),
+                ],
+              ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const StyledText(
+                    text: 'Theme',
+                  ),
+                  dropdownMenu(),
+                ],
+              ),
+            ],
+          );
+  }
+
   Widget buttons() {
     return Row(
       children: [
@@ -191,7 +317,7 @@ class _ProfilePage extends State<ProfilePage> {
           child: StyledButton(
             handlePress: () => {},
             text: 'Discard',
-            // type: 'discard',
+            type: 'discard',
           ),
         ),
         const SizedBox(width: 32),
@@ -205,78 +331,21 @@ class _ProfilePage extends State<ProfilePage> {
     );
   }
 
-  Widget preferences(User? user) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const StyledText(
-          text: 'Preferences',
-          type: 'h2',
-        ),
-        const SizedBox(height: 28),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const StyledText(text: 'Enable notifications via mail'),
-            checkbox(isMailChecked, (value) {
-              setState(() {
-                isMailChecked = value;
-              });
-            }),
-          ],
-        ),
-        const SizedBox(height: 20),
-        const StyledText(text: 'Notified Email'),
-        const SizedBox(height: 7),
-        StyledInput(
-          controller: _emailInputController,
-          hint: '${user?.email}',
-        ),
-        const SizedBox(height: 32),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const StyledText(text: 'Enable app notifications'),
-            checkbox(isNotificationChecked, (value) {
-              setState(() {
-                isNotificationChecked = value;
-              });
-            }),
-          ],
-        ),
-        const SizedBox(height: 32),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const StyledText(
-              text: 'Theme',
-            ),
-            dropdownMenu(),
-          ],
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    User? user = Provider.of<UserProvider>(context).user;
-
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         profile(user),
         const SizedBox(height: 32),
-        preferences(user),
-        const SizedBox(height: 96),
-        buttons(),
+        renderPreferences(user),
+        SizedBox(height: !areEqual ? 48 : 0),
+        !areEqual ? buttons() : Container(),
         const SizedBox(height: 64),
         StyledButton(
           handlePress: () => FirebaseAuth.instance.signOut(),
           text: 'Sign out',
+          type: 'secondary',
         )
       ],
     );
