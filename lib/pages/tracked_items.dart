6b96ffedcd36fd9085f0fb9259d892c72688b1e9
Tugce <emirmatik16@gmai.com>
2023-12-16 16:39:56 +0300

    Handle no data + prevent navigation + highlight the element + link to the original websites

--- lib/pages/tracked_items.dart
+++ lib/pages/tracked_items.dart
@@ -6,6 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:http/http.dart' as http;
 import 'package:tracky/components/styled_text.dart';
+import 'package:provider/provider.dart';
+import 'package:tracky/core/user_provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Item {
   final String uid;
@@ -48,6 +52,10 @@ class _TrackedItemsPageState extends State<TrackedItemsPage> {
     "uid": "fJARoNcmiDUFXULNhBHC39kTR552"
   };
 
+  // User? user;
+
+  bool initialLoadComplete = false;
+
   // final String serverUrl = 'http://localhost:8080';
   final String serverUrl = 'https://tracky-wwr6.onrender.com';
 
@@ -69,8 +77,17 @@ class _TrackedItemsPageState extends State<TrackedItemsPage> {
   @override
   void initState() {
     super.initState();
+    // user = Provider.of<UserProvider>(context, listen: false).user;
     items = _fetchItems();
     // TODO :: call fn to mark items as seen
+  }
+
+  Widget _textPlaceholder(String text) {
+    return StyledText(
+      text: text,
+      type: 'small',
+      color: Colors.black45,
+    );
   }
 
   Widget _webView(String url, String xpath) {
@@ -84,15 +101,38 @@ class _TrackedItemsPageState extends State<TrackedItemsPage> {
         borderRadius: const BorderRadius.all(Radius.circular(8)),
         child: InAppWebView(
           initialUrlRequest: URLRequest(url: Uri.parse(url)),
+          initialOptions: InAppWebViewGroupOptions(
+            crossPlatform: InAppWebViewOptions(
+              useShouldOverrideUrlLoading: true,
+              useShouldInterceptFetchRequest: true,
+            ),
+          ),
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            if (initialLoadComplete) {
+              return NavigationActionPolicy.CANCEL;
+            }
+
+            return NavigationActionPolicy.ALLOW;
+          },
+          shouldInterceptFetchRequest: (controller, fetchRequest) async {
+            return FetchRequest(action: FetchRequestAction.ABORT);
+          },
           gestureRecognizers: Set()
-            ..add(Factory<VerticalDragGestureRecognizer>(
-                () => VerticalDragGestureRecognizer())),
+            ..add(
+              Factory<VerticalDragGestureRecognizer>(
+                  () => VerticalDragGestureRecognizer()),
+            ),
           onLoadStop: (controller, url) async {
+            setState(() {
+              initialLoadComplete = true;
+            });
+
             controller.addJavaScriptHandler(
-                handlerName: 'getXpath',
-                callback: (args) {
-                  return xpath;
-                });
+              handlerName: 'getXpath',
+              callback: (args) {
+                return xpath;
+              },
+            );
 
             await controller.evaluateJavascript(source: '''
               (function() {
@@ -105,9 +145,16 @@ class _TrackedItemsPageState extends State<TrackedItemsPage> {
 
                   if (element) {
                     element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                    element.classList.add('selectedByTracky');
                   }
                 });
               })();
+            ''');
+
+            await controller.injectCSSCode(source: '''
+              .selectedByTracky {
+                border: 3px solid #759DEA;
+              }
             ''');
           },
         ),
@@ -116,24 +163,59 @@ class _TrackedItemsPageState extends State<TrackedItemsPage> {
   }
 
   Widget _renderTrackedItems(List<Item>? items) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: List.generate(items!.length, (index) {
-        final item = items[index];
+    return items!.isEmpty
+        ? SizedBox(
+            height: 400,
+            child: Center(
+              child: _textPlaceholder('Start tracking your favorite sites!'),
+            ),
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: List.generate(items.length, (index) {
+              final item = items[index];
+              // StyledText(text: '${item.title} - ${Uri.parse(item.url).host}'),
 
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            StyledText(text: '${item.title} - ${Uri.parse(item.url).host}'),
-            const SizedBox(height: 16),
-            _webView(item.url, item.xpath),
-            (index != items.length - 1
-                ? const SizedBox(height: 32)
-                : const SizedBox())
-          ],
-        );
-      }),
-    );
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: item.title,
+                          style: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            height: 1.5,
+                            color: Colors.black,
+                            decoration: TextDecoration.underline,
+                            decorationThickness: 2.0,
+                            decorationColor: Color.fromRGBO(0, 0, 0, 0.5),
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () async {
+                              if (await canLaunchUrl(Uri.parse(item.url))) {
+                                await launchUrl(
+                                  Uri.parse(item.url),
+                                );
+                              }
+                            },
+                        ),
+                      ),
+                      StyledText(text: ' - ${Uri.parse(item.url).host}'),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  _webView(item.url, item.xpath),
+                  (index != items.length - 1
+                      ? const SizedBox(height: 32)
+                      : const SizedBox())
+                ],
+              );
+            }),
+          );
   }
 
   @override
